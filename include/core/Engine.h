4cb71980788d267a3be2eb95923a63d78d11addf
Mahdi Madad <mahdi.madd7@gmail.com>
2026-02-23 02:49:51 +0330

    Add files via upload
    Add Engine with basic block interpreter (Move, Turn)

---
+++ include/core/Engine.h
@@ -0,0 +1,12 @@
+#ifdef ENGINE_H
+#define ENGINE_H
+#include "Block.h"
+#include "Context.h"
+class Engine {
+public:
+    void executeBlock(Block *block, Context &context);
+private:
+    void executeMove(Block *block, Context &context);
+    void executeTurn(Block *block, Context &context);
+};
+#endif
---
+++ src/core/Engine.cpp
@@ -0,0 +1,27 @@
+#include "../../include/core/Engine.h"
+#include <iostream>
+using namespace std;
+void Engine::executeBlock(Block *block, Context &context) {
+    switch (block->type) {
+        case BlockType::Move: executeMove(block, context);
+            break;
+        case BlockType::Turn: executeTurn(block, context);
+            break;
+        default: cout << "Block type not implemented yet." << endl;
+            break;
+    }
+}
+void Engine::executeMove(Block *block, Context &context) {
+    if (!block->parameters.empty()) {
+        int steps = block->parameters[0];
+        context.sprite.x += steps;
+        cout << "Move executed. New X: " << context.sprite.x << endl;
+    }
+}
+void Engine::executeTurn(Block *block, Context &context) {
+    if (!block->parameters.empty()) {
+        int angle = block->parameters[0];
+        context.sprite.direction += angle;
+        cout << "Turn executed. New direction: " << context.sprite.direction << endl;
+    }
+}
