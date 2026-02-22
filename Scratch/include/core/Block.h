e3bbb8e9fe36fe946cc5eb8fe65c8afe28a42c97
Mahdi Madad <mahdi.madd7@gmail.com>
2026-02-23 02:23:36 +0330

    Add files via upload
    Add initial Block model for Scratch engine

---
+++ Scratch/include/core/Block.h
@@ -0,0 +1,13 @@
+#ifdef BLOCK_H
+#define BLOCK_H
+#include <vector>
+#include <string>
+using namespace std;
+enum class BlockType { Move, Turn, Repeat, If, CustomFunctionCall };
+struct Block {
+    BlockType type;
+    vector<Block *> children;
+    vector<int> parameters;
+    Block(BlockType t) { type = t; }
+};
+#endif
